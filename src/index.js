463b0bc9271307d2d02881641cca991a0badaf02
Joab Owala <owalajoab@gmail.com>
2023-05-12 09:01:26 +0300

    build

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRoute as Router } from "react-router-dom"
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+<Router basename={process.env.PUBLIC_URL}>
+  <App />
+</Router>
 );
 
